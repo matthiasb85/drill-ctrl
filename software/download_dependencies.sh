2dd6d3979c901d6b339dedbc0c060921d5d0a06e
Matthias Beckert <matthias@beckert.dev>
2020-05-06 22:24:18 +0200

    added dependency to u8g2

--- software/download_dependencies.sh
+++ software/download_dependencies.sh
@@ -4,4 +4,6 @@ mkdir .3rdparty
 cd .3rdparty
 wget https://osdn.net/dl/chibios/ChibiOS_$OS_VERSION.7z
 7z x ChibiOS_$OS_VERSION.7z 
+git clone https://github.com/olikraus/u8g2.git 
+git -C u8g2 checkout 9ecb87c
 cd -
