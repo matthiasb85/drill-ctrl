1d88b35510d8f1814e850212d24c242d8fe8404c
Matthias Beckert <matthias@beckert.dev>
2020-05-25 23:02:34 +0200

    reduzed RAM usage

--- software/inc/ui/ui_cfg.h
+++ software/inc/ui/ui_cfg.h
@@ -8,7 +8,7 @@
 #ifndef INC_UI_UI_CFG_H_
 #define INC_UI_UI_CFG_H_
 
-#define UI_THREAD_STACK  2048
+#define UI_THREAD_STACK  512
 #define UI_THREAD_PRIO   (NORMALPRIO - 1)
 
 #endif /* INC_UI_UI_CFG_H_ */
